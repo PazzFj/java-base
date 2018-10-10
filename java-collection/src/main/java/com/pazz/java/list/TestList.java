86d936ff009c35276c1c2b0705454aaaad54a064
pengjian <pengjian@eshippinggateway.com>
2018-10-10 17:33:01 +0800

    update&

--- java-collection/src/main/java/com/pazz/java/list/TestList.java
+++ java-collection/src/main/java/com/pazz/java/list/TestList.java
@@ -13,16 +13,24 @@ import java.util.Vector;
 public class TestList {
 
     public static void main(String[] args) {
-        // ArrayList extends AbstractList implements List, Collection, Iterable
-        List<String> a = new ArrayList<>();
-        List<String> b = new LinkedList<>();
-        List<String> c = new Vector<>();
+        //  List<E> ==>> Collection<E> ==>> Iterable<E>
+        // ArrayList extends AbstractList<E> implements List<E>, RandomAccess, Cloneable, java.io.Serializable
+        List<String> arrayList = new ArrayList<>();
+        // LinkedList extends AbstractSequentialList<E> implements List<E>, Deque<E>, Cloneable, java.io.Serializable
+        List<String> linkedList = new LinkedList<>();
+        List<String> vectorList = new Vector<>();
+
+//        Integer s = Integer.valueOf(10);
+//        System.out.println(s);
+
+        String str2 = sun.misc.VM.getSavedProperty("com.pazz.java.list.TestClass.str1");
+        System.out.println(str2);
 
         /**
-         *                  initialCapacity     data structure      security
-         * Vector           10                  Object[]            Y
-         * LinkedList       无                   Node                N
-         * ArrayList        10                  Object[]            N
+         *                  initialCapacity     data structure      security        grow
+         * Vector           10                  Object[]            Y               2倍
+         * LinkedList       无                   Node                N               无
+         * ArrayList        10                  Object[]            N               1.5倍
          *
          */
 
