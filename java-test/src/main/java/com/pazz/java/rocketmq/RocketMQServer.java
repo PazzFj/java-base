e35a26e54937f3b1230508e9ca44a9ebc393dfd3
pengjian <pengjian@eshippinggateway.com>
2019-01-15 17:25:03 +0800

    add rocketmq test

---
+++ java-test/src/main/java/com/pazz/java/rocketmq/RocketMQServer.java
@@ -0,0 +1,34 @@
+package com.pazz.java.rocketmq;
+
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.client.producer.SendResult;
+import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.remoting.common.RemotingHelper;
+
+/**
+ * @author: 彭坚
+ * @create: 2019/1/15 10:23
+ * @description:
+ */
+public class RocketMQServer {
+
+    public static void main(String[] args) throws Exception {
+        //Instantiate with a producer group name.
+        DefaultMQProducer producer = new DefaultMQProducer("please_rename_unique_group_name");
+        // Specify name server addresses.
+        producer.setNamesrvAddr("47.101.167.134:9876");
+//        producer.setVipChannelEnabled(false);
+        //Launch the instance.
+        producer.start();
+        for (int i = 0; i < 100; i++) {
+            //Create a message instance, specifying topic, tag and message body.
+            Message msg = new Message("TopicTest", ("Hello RocketMQ " + i).getBytes(RemotingHelper.DEFAULT_CHARSET));
+            //Call send message to deliver message to one of brokers.
+            SendResult sendResult = producer.send(msg);
+            System.out.printf("%s%n", sendResult);
+        }
+        //Shut down once the producer instance is not longer in use.
+        producer.shutdown();
+    }
+
+}
