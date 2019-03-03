450bfa35b1b87bd9fa050dcb08341f116d776175
pazzf <pazzfj@163.com>
2019-03-03 23:50:45 +0800

    test method sub extends parent invoke

---
+++ java-test/src/main/java/com/pazz/java/subExt/MainTest.java
@@ -0,0 +1,15 @@
+package com.pazz.java.subExt;
+
+/**
+ * @author: 彭坚
+ * @create: 2019/3/3 22:15
+ * @description:
+ */
+public class MainTest {
+
+    public static void main(String[] args) {
+        Parent parent = new Sub();
+        parent.test();
+    }
+
+}
