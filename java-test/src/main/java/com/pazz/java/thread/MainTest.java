b29c52c5b7f5e900e836559f006de5618bb74603
pazzf <pazzfj@163.com>
2019-03-06 00:08:36 +0800

    test

---
+++ java-test/src/main/java/com/pazz/java/thread/MainTest.java
@@ -0,0 +1,17 @@
+package com.pazz.java.thread;
+
+/**
+ * @author: 彭坚
+ * @create: 2019/3/5 23:23
+ * @description:
+ */
+public class MainTest {
+
+    public static void main(String[] args) {
+        ThreadLocal<Object> local = new ThreadLocal<>();
+        local.set("aaaaaaaaa");
+
+        System.out.println((String) local.get());
+    }
+
+}
